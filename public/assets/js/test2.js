f785a2c397c109077a5f44df553e64d2e2ab46ea
Javier Ruiz <jruizard@uwaterloo.ca>
2015-09-19 23:29:09 -0400

    Fix to code retreival

--- public/assets/js/test2.js
+++ public/assets/js/test2.js
@@ -2,6 +2,6 @@ var str = window.location.href
 var keyword = "code="
 var sampleCode= "fcbcac5801c2700ac75636d8f5d9467933ac36379c942a8f5074891d7bf4e5ab"
 var len= sampleCode.length
-var n = str.indexof(keyword)
+var n = str.indexOf(keyword)
 var code = str.substring(n,n+len+5)
 console.log(code)
