43c3ed50622024fecca659264fe472186aeffc92
Javier Ruiz <jruizard@uwaterloo.ca>
2015-09-19 23:26:33 -0400

    Code Retrieval Test

--- public/assets/js/test2.js
+++ public/assets/js/test2.js
@@ -1,2 +1,7 @@
 var str = window.location.href
-console.log(str)
+var keyword = "code="
+var sampleCode= "fcbcac5801c2700ac75636d8f5d9467933ac36379c942a8f5074891d7bf4e5ab"
+var len= sampleCode.length
+var n = str.indexof(keyword)
+var code = str.substring(n,n+len+5)
+console.log(code)
