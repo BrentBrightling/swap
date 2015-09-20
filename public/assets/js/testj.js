7d3e5fb9300ac24ed25bbf05f8b011bc2435bad6
Javier Ruiz <jruizard@uwaterloo.ca>
2015-09-19 21:53:11 -0400

    Test Changes

--- public/assets/js/testj.js
+++ public/assets/js/testj.js
@@ -6,11 +6,11 @@ function httpGet(theUrl)
     return xmlHttp.responseText;
 }
 
-$(function() {
-    console.log( "ready!" );
-    $("#Login").click(function() {
-    httpGet("https://www.coinbase.com/oauth/authorize?response_type=code&client_id=5c858248b84d2a1e128055aeec577967ff45c27ea9fa782dc4f800681323f274&redirect_uri=http://www.swapexchange.co/send.html&state=SECURE_RANDOM&scope=wallet:accounts:read")
-    $("a[href='payment.html']").attr('href', 'https://www.coinbase.com/oauth/authorize?client_id=5c858248b84d2a1e128055aeec577967ff45c27ea9fa782dc4f800681323f274&redirect_uri=http%3A%2F%2Fwww.swapexchange.co%2Fsend.html&response_type=code&scope=wallet%3Auser%3Aread')
-});
-});
+//$(function() {
+//    console.log( "ready!" );
+//    $("#Login").click(function() {
+//    httpGet("https://www.coinbase.com/oauth/authorize?response_type=code&client_id=5c858248b84d2a1e128055aeec577967ff45c27ea9fa782dc4f800681323f274&redirect_uri=http://www.swapexchange.co/send.html&state=SECURE_RANDOM&scope=wallet:accounts:read")
+//    $("a[href='payment.html']").attr('href', 'https://www.coinbase.com/oauth/authorize?client_id=5c858248b84d2a1e128055aeec577967ff45c27ea9fa782dc4f800681323f274&redirect_uri=http%3A%2F%2Fwww.swapexchange.co%2Fsend.html&response_type=code&scope=wallet%3Auser%3Aread')
+//});
+//});
 
