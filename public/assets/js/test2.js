a05a2090d5328d9482bc88b70428bad8a0752ad5
Javier Ruiz <jruizard@uwaterloo.ca>
2015-09-20 04:26:33 -0400

    Added welccome message on payment page

--- public/assets/js/test2.js
+++ public/assets/js/test2.js
@@ -7,6 +7,8 @@ var codekey = str.substring(n+5,n+len+5)
 //var client = require('./client');
 console.log(codekey)
 
+var welc = document.getElementbyUD("Welcome");
+welc.text ="Welcome!"
 
 $(function() {
    $.ajax({
