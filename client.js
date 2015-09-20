ff537f9895998108a5bcb8411dabfe58f87d9b0c
Charles Bai <c3bai@uwaterloo.ca>
2015-09-20 05:27:02 -0400

    more stuff

--- client.js
+++ client.js
@@ -6,8 +6,8 @@ module.exports.init = function(acc_token, refresh_token) {
     'refreshToken': refresh_token,
   });
   console.log(acc_token + "     " + refresh_token);
-  showBalance();
-  //makeTransfer();
+  //showBalance();
+  //transfer();
 }
 
 //Displays the value of the balance
@@ -21,22 +21,31 @@ var showBalance = function() {
   });
 }
 
-//Transfer function
-// var makeTransfer = function () {
-//   client.getBuyPrice({'qty': 1, 'currency': 'USD'}, function(err, obj) {
-//     console.log('total amount: ' + obj.total.amount);
-//   });
-// }
-//
-// module.exports.testTransfer = function() {
-//   var Client = require('coinbase').Client;
-//   var client = new Client({
-//     'apiKey' : 'G8ZHFjQAthZATXsl',
-//     'apiSecret' : 'D48wh5yNujyfj7mQeSTi2I8tLnPgow4V',
-//     'baseApiUri': 'https://api.sandbox.coinbase.com/v1/',
-//     'tokenUri': 'https://api.sandbox.coinbase.com/oauth/token'
-//   });
-//   client.getBuyPrice({'qty': 1, 'currency': 'USD'}, function(err, obj) {
-//     console.log('total amount: ' + obj.total.amount);
-//   });
-// }
+var makeTransfer = function() {
+  var args = {
+    "to": "user1@example.com",
+    "amount": "1.234",
+    "notes": "Sample transaction for you"
+  };
+  account.sendMoney(args, function(err, txn) {
+    console.log('my txn id is: ' + txn.id);
+  });
+}
+
+var transfer = function() {
+  // var Client = require('coinbase').Client;
+  // var client = new Client({
+  //   'apiKey' : 'G8ZHFjQAthZATXsl',
+  //   'apiSecret' : 'D48wh5yNujyfj7mQeSTi2I8tLnPgow4V',
+  //   'baseApiUri': 'https://api.sandbox.coinbase.com/v1/',
+  //   'tokenUri': 'https://api.sandbox.coinbase.com/oauth/token'
+  // });
+  client.getBuyPrice({'qty': 1, 'currency': 'CAN'}, function(err, obj) {
+    dollarsToBit(obj.total.amount)
+  });
+}
+
+var dollarsToBit = function (bitCoinValue) {
+  var noOfBitCoins = 0.50 / bitCoinValue;
+  console.log(noOfBitCoins);
+}
