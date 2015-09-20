16325288c673abf0a55ed71102b09a106688186e
Javier Ruiz <jruizard@uwaterloo.ca>
2015-09-20 02:33:19 -0400

    1

--- public/assets/js/test2.js
+++ public/assets/js/test2.js
@@ -9,16 +9,8 @@ console.log(codekey)
 
 $(function() {
    $.ajax({
-			url: "https://api.coinbase.com/oauth/token",
-			type: "POST",
-			dataType: 'jsonp',
-			data: {
-				grant_type: "authorization_code", 
-				code: codekey,
-				client_id: "5c858248b84d2a1e128055aeec577967ff45c27ea9fa782dc4f800681323f274",
-				client_secret: "caff1da29f54829a81afd11e5bdb41d7ac263e5b396b994ab0136e50af1eecb9",
-				redirect_uri: "http://www.swapexchange.co/payment.html"
-			},
+			url: "https://whispering-earth-7145.herokuapp.com/" + codekey,
+			type: "GET",
 			success: function(data) {
 				alert(data);
 			},
@@ -31,6 +23,8 @@ $(function() {
 
 
 
-curl https://api.coinbase.com/oauth/token \
-  -X POST \
-  -d 'grant_type=authorization_code&code=4e81c79a5c55b334bc695df5ac4b27fa95f5b999d0a4713f8c007ead6d5616fe&client_id=5c858248b84d2a1e128055aeec577967ff45c27ea9fa782dc4f800681323f274&client_secret=caff1da29f54829a81afd11e5bdb41d7ac263e5b396b994ab0136e50af1eecb9&redirect_uri=http://www.swapexchange.co/payment.html'
+//curl https://api.coinbase.com/oauth/token \
+//  -X POST \
+//  -d 'grant_type=authorization_code&code=4e81c79a5c55b334bc695df5ac4b27fa95f5b999d0a4713f8c007ead6d5616fe&client_id=5c858248b84d2a1e128055aeec577967ff45c27ea9fa782dc4f800681323f274&client_secret=caff1da29f54829a81afd11e5bdb41d7ac263e5b396b994ab0136e50af1eecb9&redirect_uri=http://www.swapexchange.co/payment.html'
+
+
